af8e45b4206cb1bdb85144b16eb697b0ac7f0913
Justin de Ruiter <djjustin13@gmail.com>
2018-07-13 17:54:54 +0200

    Scaled ui down

--- docs/js/main.js
+++ docs/js/main.js
@@ -197,16 +197,16 @@ var Shop = (function () {
             src: ['sounds/money.mp3']
         });
         this.items.push(new ShopItem(this, this.block, "Student", "user", "Koop student | ", 1, 35));
-        this.items.push(new ShopItem(this, this.block, "Peercoach", "user-graduate", "Koop peercoach | ", 5, 100));
-        this.items.push(new ShopItem(this, this.block, "Group", "users", "Koop klas | ", 10, 165));
-        this.items.push(new ShopItem(this, this.block, "Teacher", "user-tie", "Koop docent | ", 25, 230));
-        this.items.push(new ShopItem(this, this.block, "School", "school", "Koop school | ", 100, 295));
-        this.items.push(new ShopItem(this, this.block, "Building", "building", "Koop bedrijf | ", 300, 360));
-        this.items.push(new ShopItem(this, this.block, "Farm", "seedling", "Koop farm | ", 800, 425));
-        this.items.push(new ShopItem(this, this.block, "Factory", "industry", "Koop fabriek | ", 2400, 490));
-        this.items.push(new ShopItem(this, this.block, "Laboratory", "flask", "Koop lab | ", 7500, 555));
-        this.items.push(new ShopItem(this, this.block, "Country", "chess-king", "Koop land | ", 22500, 620));
-        this.items.push(new ShopItem(this, this.block, "World", "globe", "Koop wereld | ", 85000, 685));
+        this.items.push(new ShopItem(this, this.block, "Peercoach", "user-graduate", "Koop peercoach | ", 5, 90));
+        this.items.push(new ShopItem(this, this.block, "Group", "users", "Koop klas | ", 10, 145));
+        this.items.push(new ShopItem(this, this.block, "Teacher", "user-tie", "Koop docent | ", 25, 200));
+        this.items.push(new ShopItem(this, this.block, "School", "school", "Koop school | ", 100, 255));
+        this.items.push(new ShopItem(this, this.block, "Building", "building", "Koop bedrijf | ", 300, 310));
+        this.items.push(new ShopItem(this, this.block, "Farm", "seedling", "Koop farm | ", 800, 365));
+        this.items.push(new ShopItem(this, this.block, "Factory", "industry", "Koop fabriek | ", 2400, 420));
+        this.items.push(new ShopItem(this, this.block, "Laboratory", "flask", "Koop lab | ", 7500, 475));
+        this.items.push(new ShopItem(this, this.block, "Country", "chess-king", "Koop land | ", 22500, 530));
+        this.items.push(new ShopItem(this, this.block, "World", "globe", "Koop wereld | ", 85000, 585));
     }
     Shop.prototype.update = function () {
         for (var _i = 0, _a = this.items; _i < _a.length; _i++) {
